29dec2d66c4bbb7b5f01b6de4d2c62341c1737cc
Sergio Mejia <sdmejiac@gmail.com>
2023-02-19 22:59:23 -0500

    Crear y agregar categorias a la lista

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([ 'The Office', 'React']);
+
+    const onAddCategory = () => {
+        setCategories([ 'Premier League', ...categories]);
+    }
+
   return (
     <>
-    
-        <h1>GifExpertApp</h1>
+      <h1>GifExpertApp</h1>
+
+
+      {/* listado de Gif */}
+      <button onClick={ onAddCategory }>Agregar</button>
+      <ol>
+        { categories.map( category => {
+            return <li key={ category }>{category}</li>
+        })}
+      </ol>
     </>
-  )
-}
+  );
+};
