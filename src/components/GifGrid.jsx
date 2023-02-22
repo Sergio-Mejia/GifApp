9bb38adb572ca47fb62f6ab939b327ef270be0d0
Sergio Mejia <sdmejiac@gmail.com>
2023-02-21 22:49:24 -0500

    Mostrar titulos de las imagenes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,17 +1,26 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getGifts } from "../helpers/getGifs";
 
-
 export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
 
-  useEffect( () => {
-    getGifts(category);
+  const getImages = async () => {
+    const newImage = await getGifts(category);
+    setImages(newImage);
+  };
+
+  useEffect(() => {
+    getImages();
   }, []);
-
 
   return (
     <>
       <h3>{category}</h3>
+      <ol>
+        {images.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ol>
     </>
   );
 };
